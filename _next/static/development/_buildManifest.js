17a6ac97d6dbbb7d8e9277869140fc1e0eef263f
arswarog <arswarog@users.noreply.github.com>
2021-06-12 16:37:39 +0000

    deploy: chunk-db/chunk-db@e1ccbb4ff7ab3ea6afe6f05bbc8ea4891276ab8d

--- _next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/todolist":["static\u002Fchunks\u002Fpages\u002Ftodolist.js"],sortedPages:["\u002F_app","\u002F_error","\u002Ftodolist"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
